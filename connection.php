a83ada3e7f66c5c7aa4e43e95393f0eb76165382
Tan Szu Jean <szujean45@gmail.com>
2022-07-21 11:14:49 +0800

    Add form query to submit ticket

--- connection.php
+++ connection.php
@@ -1,16 +1,15 @@
 <?php
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
-
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-if(!$conn) {
-    die("Connection failed: ".mysqli_connect_error());
-}
+  $db_host = 'localhost';
+  $db_user = 'root';
+  $db_password = 'root';
+  $db_db = 'foodunity';
+ 
+  $db = mysqli_connect($db_host, $db_user, $db_password, $db_db);
+	
+  if ($db===false) {
+    echo 'Errno: '.$mysqli->connect_errno;
+    echo '<br>';
+    echo 'Error: '.$mysqli->connect_error;
+    exit();
+  }
+?>
--- restaurantStaff.php
+++ restaurantStaff.php
@@ -1,7 +1,6 @@
 <?php
 include 'connection.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -24,90 +23,125 @@ include 'connection.php';
                 <div class=" wow fadeInDown" data-wow-delay="0.3s"></div>
             </div>
             <div class="container">
-                    <div class="col-sm-12">
-                        <div class="card">
-                            <div class="card-body">
-                                <form class="needs-validation" novalidate>
-                                    <h4 class="text-center form-section-title">Restaurant's Detail</h4>
-                                    <div class="shape form-shape"></div>
-                                    <div class="form-row">
-                                        <div class="col-md-12 mb-3">
-                                            <label for="validationCustom01" class="form-input-title"><b>Restaurant Name</b></label>
-                                            <input type="text" class="form-control" id="validationCustom01" required>
-                                            <div class="valid-feedback">
-                                                Looks good!
-                                            </div>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid restaurant name.
-                                            </div>
+                <div class="col-sm-12">
+                    <div class="card wow fadeInUp">
+                        <div class="card-body">
+                            <?php
+                            // Add Data
+                            if (isset($_POST["submitTicket"])) {
+                                $resName = $_POST["restaurantName"];
+                                $resTime = $_POST["restaurantTime"];
+                                $resAddress = $_POST["restaurantAddress"];
+                                $restState = $_POST["restaurantState"];
+                                $resZip = $_POST["restaurantZip"];
+                                $subName = $_POST["submitterName"];
+                                $subNumber = $_POST["submitterNumber"];
+                                $subDesign = $_POST["submitterDesignation"];
+
+                                $sql = "INSERT INTO restaurantTicket (ticketRestaurantName,ticketRestaurantTime,ticketRestaurantAddress,ticketRestaurantState,ticketRestaurantZip,ticketSubmitterName,ticketSubmitterNumber,ticketSubmitterDesignation) 
+VALUES ('$resName','$resTime','$resAddress','$restState','$resZip','$subName','$subNumber','$subDesign')";
+                                $result = mysqli_query($db, $sql);
+
+                                if ($result) {
+                                    echo '<div class="alert alert-success" role="alert">
+Ticket submitted successfully.</div>';
+                                } else {
+                                    echo '<div class="alert alert-danger" role="alert">
+Ticket is not submitted</div>';
+                                    echo mysqli_error($db);
+                                }
+                            } ?>
+                            <form class="needs-validation" novalidate action="" method="POST">
+                                <h4 class="text-center form-section-title">Restaurant's Detail</h4>
+                                <div class="shape form-shape"></div>
+                                <div class="form-row">
+                                    <div class="col-md-8 mb-3">
+                                        <label for="restaurantName" class="form-input-title"><b>Restaurant Name</b></label>
+                                        <input type="text" class="form-control" name="restaurantName" required>
+                                        <div class="valid-feedback">
+                                            Looks good!
+                                        </div>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid restaurant name.
                                         </div>
                                     </div>
-                                    <div class="form-row">
-                                        <div class="col-md-6 mb-3">
-                                            <label for="validationCustom03" class="form-input-title"><b>Restaurant Address</b></label>
-                                            <input type="text" class="form-control" id="validationCustom03" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid address.
-                                            </div>
+                                    <div class="col-md-4 mb-3">
+                                        <label for="restaurantTime" class="form-input-title"><b>What time will you stay until?</b></label>
+                                        <input type="time" class="form-control" name="restaurantTime" required>
+                                        <div class="valid-feedback">
+                                            Looks good!
                                         </div>
-                                        <div class="col-md-3 mb-3">
-                                            <label for="validationCustom04" class="form-input-title"><b>State</b></label>
-                                            <input type="text" class="form-control" id="validationCustom04" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid state.
-                                            </div>
-                                        </div>
-                                        <div class="col-md-3 mb-3">
-                                            <label for="validationCustom05" class="form-input-title"><b>Zip</b></label>
-                                            <input type="text" class="form-control" id="validationCustom05" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid zip.
-                                            </div>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid restaurant name.
                                         </div>
                                     </div>
-                                    <h4 class="text-center form-section-title">Submitter's Detail</h4>
-                                    <div class="shape form-shape"></div>
-                                    <div class="form-row">
-                                        <div class="col-md-4 mb-3">
-                                            <label for="validationCustom03" class="form-input-title"><b>Name</b></label>
-                                            <input type="text" class="form-control" id="validationCustom03" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid name.
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 mb-3">
-                                            <label for="validationCustom04" class="form-input-title"><b>Phone Number</b></label>
-                                            <input type="text" class="form-control" id="validationCustom04" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid state.
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4 mb-3">
-                                            <label for="validationCustom05" class="form-input-title"><b>Designation</b></label>
-                                            <input type="text" class="form-control" id="validationCustom05" required>
-                                            <div class="invalid-feedback">
-                                                Please provide a valid zip.
-                                            </div>
+                                </div>
+                                <div class="form-row">
+                                    <div class="col-md-6 mb-3">
+                                        <label for="restaurantAddress" class="form-input-title"><b>Restaurant Address</b></label>
+                                        <input type="text" class="form-control" name="restaurantAddress" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid address.
                                         </div>
                                     </div>
-                                    <div class="form-group">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
-                                            <label class="form-check-label" for="invalidCheck">
-                                                Agree to terms and conditions
-                                            </label>
-                                            <div class="invalid-feedback">
-                                                You must agree before submitting.
-                                            </div>
+                                    <div class="col-md-3 mb-3">
+                                        <label for="restaurantState" class="form-input-title"><b>State</b></label>
+                                        <input type="text" class="form-control" name="restaurantState" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid state.
                                         </div>
                                     </div>
-                                    <button class="btn btn-primary" type="submit">Submit Food Unity Champaign Ticket</button>
-                                </form>
-                            </div>
+                                    <div class="col-md-3 mb-3">
+                                        <label for="restaurantZip" class="form-input-title"><b>Zip</b></label>
+                                        <input type="number" class="form-control" name="restaurantZip" min="11111" max="99999" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid zip.
+                                        </div>
+                                    </div>
+                                </div>
+                                <h4 class="text-center form-section-title">Submitter's Detail</h4>
+                                <div class="shape form-shape"></div>
+                                <div class="form-row">
+                                    <div class="col-md-4 mb-3">
+                                        <label for="submitterName" class="form-input-title"><b>Name</b></label>
+                                        <input type="text" class="form-control" name="submitterName" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid name.
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <label for="submitterNumber" class="form-input-title"><b>Phone Number</b></label>
+                                        <input type="tel" class="form-control" name="submitterNumber" pattern="[0-9]{11}" placeholder="01234398454" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid state.
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 mb-3">
+                                        <label for="submitterDesignation" class="form-input-title"><b>Designation</b></label>
+                                        <input type="text" class="form-control" name="submitterDesignation" required>
+                                        <div class="invalid-feedback">
+                                            Please provide a valid zip.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                                        <label class="form-check-label" for="invalidCheck">
+                                            Agree to terms and conditions
+                                        </label>
+                                        <div class="invalid-feedback">
+                                            You must agree before submitting.
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="btn btn-primary" type="submit" name="submitTicket">Submit Food Unity Champaign Ticket</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
         </div>
 
         <!-- Preloader -->
