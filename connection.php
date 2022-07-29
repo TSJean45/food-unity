b99bd8d74c99d0d6141cbb9d08fd477669f227a1
Tan Szu Jean <szujean45@gmail.com>
2022-07-30 01:04:18 +0800

    Change database connection to heroku remote

--- connection.php
+++ connection.php
@@ -1,15 +1,12 @@
 <?php
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_password = 'root';
-  $db_db = 'foodunity';
- 
-  $db = mysqli_connect($db_host, $db_user, $db_password, $db_db);
-	
-  if ($db===false) {
-    echo 'Errno: '.$mysqli->connect_errno;
-    echo '<br>';
-    echo 'Error: '.$mysqli->connect_error;
-    exit();
-  }
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
